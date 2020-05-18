dd0825b531226f2ec9bb911af5a226ae9ce5cc90
Joffrey Fortin <fortinjoffrey@gmail.com>
2020-05-18 15:56:28 +0200

    Initial files

---
+++ CircularLoader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircularLoader
+//
+//  Created by Joffrey Fortin on 18/05/2020.
+//  Copyright © 2020 Joffrey Fortin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
