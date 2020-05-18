8369495d6db34c7d00106eec70b8baab2268b2c5
Joffrey Fortin <fortinjoffrey@gmail.com>
2020-05-18 16:17:44 +0200

    001 Shape Layers, Percentage Label
    - 1 Shape layer for future pulsating animation
    - 1 Shape Layer above
    - 1 Label to represent future %

--- CircularLoader/ViewController.swift
+++ CircularLoader/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  CircularLoader
+//  CircularLoaderLBTA
 //
 //  Created by Joffrey Fortin on 18/05/2020.
 //  Copyright © 2020 Joffrey Fortin. All rights reserved.
@@ -9,12 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var shapeLayer = CAShapeLayer()
+    
+    var pulsatingLayer: CAShapeLayer!
+    
+    let percentageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        return label
+    }()
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .backgroundColor
+        
+        setupCircleLayers()
+        setupPercentageLabel()
     }
-
-
+    
+    private func setupCircleLayers() {
+        
+        pulsatingLayer = createCircleShapeLayer(strokeColor: .clear, fillColor: .pulsatingFillColor)
+        _ = createCircleShapeLayer(strokeColor: .trackStrokeColor, fillColor: .backgroundColor)
+    }
+    
+    fileprivate func setupPercentageLabel() {
+        view.addSubview(percentageLabel)
+        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        percentageLabel.center = view.center
+    }
+    
+    private func createCircleShapeLayer(strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        layer.path = circularPath.cgPath
+        layer.strokeColor = strokeColor.cgColor
+        layer.lineWidth = 20
+        layer.fillColor = fillColor.cgColor
+        layer.lineCap = CAShapeLayerLineCap.round
+        layer.position = view.center
+        view.layer.addSublayer(layer)
+        return layer
+    }
 }
+
+
 
